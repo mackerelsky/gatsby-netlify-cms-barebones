2b3461dcb2e5d29f3005c6492107d56c33b00c6d
Ed Goode <ed@mackerelsky.co.nz>
2020-01-07 14:39:38 +1300

    apply styles through the layout template

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Footer from "./footer"
+import "../styles/style.css"
 
 const Layout = ({ children }) => {
     const data = useStaticQuery(graphql`
